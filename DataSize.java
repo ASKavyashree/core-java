cebac253acb39c97de36a9296db5300dbe55484d
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:22:41 +0530

    Adding a new file

---
+++ DataSize.java
@@ -0,0 +1,24 @@
+class DataSize{
+    public static void main(String args[]){
+    // formula -2(n-1) to 2^(n-1) -1
+    byte a=127;//-128 to 127
+    short s = 32767;
+    int speed = 2147483647;
+    long salary = 21474836648l;
+    float f=34.4f;
+    double d=23.5d;
+    System.out.println(a);
+    System.out.println(s);
+    System.out.println(speed);
+    //int 4 bytes=32 bits
+    //1byte = 8 bit
+    // long = 8 bytes
+    //float =4 bytes
+    //double =8 bytes
+    //boolean = 1bit
+    //char = 2 bytes
+    //byte = 1 byte = 8bit
+    //short = 2 byte =  16 bits
+    
+    }
+}
