f6672ae832bc7269a180ce7b775fc9b1ff9e4fa0
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:17:23 +0530

    Adding a new file

---
+++ Book.java
@@ -0,0 +1,14 @@
+class Book {
+    public static void main(String[] args){
+        System.out.println("Book Title");
+        System.out.println("Author Name");
+        System.out.println("Year of publication");
+        System.out.println("Sold of Books");
+        System.out.println("Types of themes");
+        System.out.println("Language of Book");
+        System.out.println("What is the story of book");
+        System.out.println("What is aim of this book");
+        System.out.println("Rating of this book");
+        System.out.println("What is the Number of the users");
+    }
+}
