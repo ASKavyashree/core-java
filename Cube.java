e789a2e73b949edb037b3fb64ad03841bc5651a5
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 12:32:31 +0530

    Adding the file

---
+++ Cube.java
@@ -0,0 +1,11 @@
+public class Cube {
+    public static void main(String[] args){
+        int n=3;
+        cube(n);
+    }
+    public static int cube(int n){
+        int result=n*n*n;
+        System.out.println(result);
+        return result;
+    }
+}
