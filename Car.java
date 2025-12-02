fc3a1148f3e4796246f4300722aad063bc91954b
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:18:57 +0530

    Adding a new file

---
+++ Car.java
@@ -0,0 +1,14 @@
+class Car{
+    public static void main(String[] args){
+        System.out.println("Speed of Car");
+        System.out.println("Color of Car");
+        System.out.println("Model of Car");
+        System.out.println("Year of Car");
+        System.out.println("Top Speed of vehicle:");
+        System.out.println("Number of door in a Vehical");
+        System.out.println("Number of seats");
+        System.out.println("It is as a Unqiue Registration Number");
+        System.out.println("What is the top speed");
+        System.out.println("What is the current status of engine level");
+    }
+}
