57aa70f6d3407411b8afbea6654461e8e2681020
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:27:49 +0530

    Adding new file

---
+++ Static1.java
@@ -0,0 +1,35 @@
+public class Static1 {
+    public static void main(String args[]){
+        addition();
+        subtraction();
+        multiply();
+        division();
+        concation();
+
+    }
+    public static void addition(){
+        int a =10;
+        int b =5;
+        System.out.println(a+b); 
+    }
+    public static void subtraction(){
+        int c=20;
+        int d=10;
+        System.out.println(c-d);
+    }
+    public static void multiply(){
+        int e=10;
+        int f=15;
+        System.out.println(e*f);
+    } 
+    public static void division(){
+        int g=120;
+        int h=6;
+        System.out.println(g/h);
+    }
+    public static void concation(){
+        String i="java";
+        String j="programming";
+        System.out.println(i+j);
+    }
+}
