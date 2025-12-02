48ef15488125830cd875b4ef7473a7a19bcaed77
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:28:30 +0530

    Adding new file

---
+++ Static2.java
@@ -0,0 +1,20 @@
+public class Static2 {
+    public static void main(String[] args){
+        Static2.walking();
+        Static2.dancing();
+        Static2.running();
+        Static2.working();
+    }
+    public static void walking(){
+        System.out.println("walking on the park ");
+    }
+    public static void dancing(){
+        System.out.println("Dancing on the floor");
+    }
+    public static void running(){
+        System.out.println("Running");
+    }
+    public static void working(){
+        System.out.println("working ");
+    }
+}
