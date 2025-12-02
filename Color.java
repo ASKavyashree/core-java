df284c1a1ee2237b0020dc85f449aa503625f869
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:20:46 +0530

    adding new file

---
+++ Color.java
@@ -0,0 +1,13 @@
+public class Color {
+     public static void main(String[] args){
+        System.out.print("Red");
+        System.out.print("Blue");
+        System.out.print("Yellow");
+        System.out.print("Purple");
+        System.out.print("Orange");
+        System.out.print("White");
+        System.out.print("Black");
+        System.out.print("Pink");
+        System.out.print("Brown");
+    }
+}
