cb825911035975921f8aaf1fe9c62e4b391446b4
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:35:54 +0530

    Adding a new file

---
+++ Myclass.java
@@ -0,0 +1,6 @@
+class Myclass{
+    public static void main(String[] args){
+        System.out.println("Welcome to java");
+        System.out.println("We are learning to how to create java file and execute/run the program");
+    }
+}
