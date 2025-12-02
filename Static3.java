0ed882fc9366badc8470a6fed1de7e4b9aa0513b
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:29:09 +0530

    Adding a new file

---
+++ Static3.java
@@ -0,0 +1,20 @@
+public class Static3 {
+    public static void main(String[] args){
+        Static3.department();
+        Static3.facutly();
+        Static3.student();
+        Static3.syballus();
+    }
+    public static void department(){
+        System.out.println("CSA Department");
+    }
+    public static void student(){
+        System.out.println("She is belong to CSA ");
+    }
+    public static void facutly(){
+        System.out.println("Teaching");
+    }
+    public static void syballus(){
+        System.out.println("4 Units/modules");
+    }
+}
