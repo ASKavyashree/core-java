260c52e485a0785cab089178d476bf735bc79326
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:15:49 +0530

    Adding new file

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    public static void main(String args[]){
+        System.out.println("Types of Animals");
+        System.out.println("Domestic Animals");
+        System.out.println("Wild Animals");
+        System.out.println("Domestic will eat the  Plants");
+        System.out.println("Wild Animals will eat the meat");
+        System.out.println("Domestic animals will also called herbivores ");
+        System.out.println("Wild animals will also called carnivores");
+        System.out.println("If both eat a plant and meat are called as omnivores");
+        System.out.println("Examples of Domestic animals are Cow , Goat etc");
+        System.out.println("Examples of Wild animals are  Lion,Tiger");
+    }
+}
+
