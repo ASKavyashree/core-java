a491526603e195daa36e6dad668eea57a0cef81e
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:37:38 +0530

    Adding a new file

---
+++ Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    public static void main(String[] args){
+        System.out.println("Product Name");
+        System.out.println("Product id");
+        System.out.println("Product Sector");
+        System.out.println("Product items");
+        System.out.println("Adding the details");
+        System.out.println("Removing the Products");
+        System.out.println("Sending the products");
+        System.out.println("Total number of product");
+
+    }
+}
