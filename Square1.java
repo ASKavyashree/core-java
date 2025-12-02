e06092787405bb414f4ce2297f65323dde9e442f
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:33:45 +0530

    Adding a new file

---
+++ Square1.java
@@ -0,0 +1,12 @@
+public class Square1 {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        square(a,b);
+    }
+    public static int square(int a,int b){
+        int result=(a+b)*2;
+        System.out.println(result);
+        return result;
+    }
+}
