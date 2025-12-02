fab64a2cd8a2545c02ebea47ca5b4860ed0be6d6
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:35:20 +0530

    adding new file

---
+++ Employee.java
@@ -0,0 +1,9 @@
+class Employee {
+ public static void main(String args[]){
+    System.out.println("Employee name");
+    System.out.println("Employee Department");
+    System.out.println("Salary");
+    System.out.println("Experencies");
+    System.out.println("Post");
+ }   
+}
