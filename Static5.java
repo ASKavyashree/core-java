ef4b93a81c45d25d0c7117928884d681f440e640
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:30:38 +0530

    Adding a new file

---
+++ Static5.java
@@ -0,0 +1,24 @@
+public class Static5 {
+    public static void main(String args[]){
+        Static5.computerFeature1();
+        Static5.computerFeature2();
+        Static5.computerFeature3();
+        Static5.computerFeature4();
+        Static5.computerFeature5();
+    }
+    public static void computerFeature1(){
+        System.out.println("Input feature");
+    }
+    public static void computerFeature2(){
+        System.out.println("Output feature");
+    }
+    public static void computerFeature3(){
+        System.out.println("Hardware System");
+    }
+    public static void computerFeature4(){
+        System.out.println("Software System");
+    }
+    public static void computerFeature5(){
+        System.out.println("Memory System");
+    }
+}
