9df2540cc04da547e520ef81f96387ba26090aea
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:37:02 +0530

    Adding a new file

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+   public static void main(String args[]){
+    System.out.println("Person Name");
+    System.out.println("Person id");
+    System.out.println("Person Full Name");
+    System.out.println("Last Name");
+    System.out.println("Age");
+    System.out.println("Address");
+    System.out.println("Working or Not");
+    System.out.println("Salary");
+   }
+}
