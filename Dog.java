42846d394727e4c4b958de5238397495746b81fc
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 12:39:48 +0530

    Adding a new file

---
+++ Dog.java
@@ -0,0 +1,17 @@
+public class Dog{
+     
+    public static void main(String[] args){
+        System.out.print("Eating");
+        System.out.print("Sleeping");
+        System.out.print("Barking");
+        System.out.print("Walking ");
+        System.out.print("Danceing");
+        System.out.print("Hearing");
+        System.out.print("Dog Name");
+
+    }
+
+}
+
+
+
