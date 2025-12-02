d8b854513992c590152bd74f432741e8c7bd643a
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:36:26 +0530

    Adding a new file

---
+++ Simple.java
@@ -0,0 +1,12 @@
+public class Simple {
+    public static void main(String args[]){
+        System.out.println("Iam Trainee");
+        System.out.println("Class is taken by Trainer");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(5+50);
+        System.out.println(5*50);
+        System.out.println("Reading input from command line");
+        System.out.println(args[0]+args[1]+args[2]);
+    }
+}
