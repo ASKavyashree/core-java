65de1e8fa9b07539a04d4b3e32450f9b1aae07a9
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 12:16:33 +0530

    Adding a new file

---
+++ Number.java
@@ -0,0 +1,12 @@
+class Number{
+    public static void main(String[] args){
+        System.out.print("Even Number");
+        System.out.print("Odd Number");
+        System.out.print("Whole Number");
+        System.out.print("Integer Number");
+        System.out.print("Real Number");
+        System.out.print("Natural Number");
+        System.out.print("Rational Number");
+        System.out.print("Complex Number");
+    }
+}
