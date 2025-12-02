adf2cf1064bbe8c81a35356348a816e41b9b8ded
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:29:54 +0530

    Adding a new file

---
+++ Static4.java
@@ -0,0 +1,20 @@
+public class Static4 {
+    public static void main(String[]  args){
+        Static4.product1();
+        Static4.product2();
+        Static4.product3();
+        Static4.product4();
+    }
+    public static void product1(){
+        System.out.println("Quality of Good");
+    }
+    public static void product2(){
+        System.out.println("Quality is not good");
+    }
+    public static void product3(){
+        System.out.println("Quality is average");
+    }
+    public static void product4(){
+        System.out.println("Quality is not bad");
+    }
+}
