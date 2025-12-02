2e41fd4e1f233671f051b660cc0dff1781c01e6d
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:21:49 +0530

    Adding a new file

---
+++ Calculations.java
@@ -0,0 +1,51 @@
+class Calculations{
+    public static void main(String[] args){
+        int a=40;
+        int b=20;
+        float c=12.345f;
+        float d=2.8f;
+        String name="ABC";
+        char var='f';
+        boolean e=true;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(c+d);
+        System.out.println(c-d);
+        System.out.println(c*d);
+        System.out.println(c/d);
+        System.out.println(c%d);
+        System.out.println(a+name);
+        //System.out.println(a-name); //Complication error
+        //System.out.println(a*name); Complication error
+        //System.out.println(a/name); Complication error
+        //System.out.println(a*name); Complication error
+       // System.out.println(c+name);
+        //System.out.println(c-name); Complication error
+        //System.out.println(c*name); Complication error
+        //System.out.println(c/name); Complication error
+        //System.out.println(c%name); Complication error
+        System.out.println(a+var);
+        System.out.println(a-var);
+        System.out.println(a*var); //ascii value
+        System.out.println(a/var);
+        System.out.println(a%var);
+        System.out.println(d+var);
+        System.out.println(d-var);
+        System.out.println(d*var);
+        System.out.println(d/var);
+        System.out.println(d%var);
+        //System.out.println(a+e); Complication error
+        //System.out.println(a-e); Complication error
+        //System.out.println(a*e); Complication error
+        //System.out.println(a/e); Complication error
+        //System.out.println(a%e); Complication error
+        //System.out.println(d+e); Complication error
+        //System.out.println(d-e); Complication error
+        //System.out.println(d*e); Complication error
+        //System.out.println(d/e); Complication error
+        //System.out.println(d%e); Complication error
+    }
+}
