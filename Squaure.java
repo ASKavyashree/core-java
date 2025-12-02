3bcfd91b6eeeaff5c15ad0ce65bd14ed6e8c581d
ASKavyashree <askavyashree22@gmail.com>
2025-12-02 22:34:28 +0530

    Adding a new file

---
+++ Squaure.java
@@ -0,0 +1,11 @@
+class Squaure{
+    public static void main(String args[]){
+        int  num1=4;
+        square(num1);
+    }
+    public static int square(int num1){
+        int result=num1*num1;
+        System.out.println(result);
+        return result;
+    }
+}
