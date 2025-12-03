bd8a13b66a37f66703c49b0cdac3e72c1862e1d5
ASKavyashree <askavyashree22@gmail.com>
2025-12-03 10:48:11 +0530

    Adding a new file

---
+++ School.java
@@ -0,0 +1,11 @@
+public class School {
+    public static void main(String[] args){
+        System.out.println("School of fondation of year");
+        System.out.println("Number of Students in each class");
+        System.out.println("Total number of teacher");
+        System.out.println("How many Subjects");
+        System.out.println("How many employee of School");
+        System.out.println("School founder Name");
+        System.out.println("How many class are there in School");
+    }
+}
