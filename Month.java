3b19fc81a6565c1bd33bfa456f26ca0414dcf287
ASKavyashree <askavyashree22@gmail.com>
2025-12-04 19:18:45 +0530

    Adding a Month class

---
+++ Month.java
@@ -0,0 +1,31 @@
+public class Month {
+    public static void month(int value){
+        if(value==1){
+            System.out.println("January");
+        }else if(value==2){
+            System.out.println("February");
+        }else if(value==3){
+            System.out.println("March");
+        }else if(value==4){
+            System.out.println("April");
+        }else if(value==5){
+            System.out.println("May");
+        }else if(value==6){
+            System.out.println("June");
+        }else if(value==7){
+            System.out.println("July");
+        }else if(value==8){
+            System.out.println("August");
+        }else if(value==9){
+            System.out.println("September");
+        }else if(value==10){
+            System.out.println("October");
+        }else if(value==11){
+            System.out.println("November");
+        }else if(value==12){
+            System.out.println("December");
+        }else{
+            System.out.println("Invaild Value");
+        }
+    }
+}
