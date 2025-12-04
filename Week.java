8997c6caf1cfa7d8999603676523653046ebdc17
ASKavyashree <askavyashree22@gmail.com>
2025-12-04 19:24:07 +0530

    Adding a week file

---
+++ Week.java
@@ -0,0 +1,19 @@
+public class Week {
+    public static void week(int week){
+        if(week==0){
+            System.out.println("Sunday");
+        }else if(week==1){
+            System.out.println("Monday");
+        }else if(week==2){
+            System.out.println("Tuesday");
+        }else if(week==3){
+            System.out.println("Wednesday");
+        }else if(week==4){
+            System.out.println("Thursday");
+        }else if(week==5){
+            System.out.println("Friday");
+        }else if(week==6){
+            System.out.println("Saturday");
+        }
+    }
+}
