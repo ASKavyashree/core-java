87c52b75eeec3fd4d94fd1ae9dc26da19d3a4dcb
ASKavyashree <askavyashree22@gmail.com>
2025-12-18 20:05:55 +0530

    Adding a new file

---
+++ BagObject.java
@@ -0,0 +1,12 @@
+public class BagObject {
+    static int cost=200;
+    String type="Handbag";
+    public static void main(String args[]){
+        BagObject bg = new BagObject();
+        System.out.println(bg.cost);
+
+        BagObject bg1 = new BagObject();
+        System.out.println(bg1.type);
+
+    }
+}
