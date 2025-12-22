4551690ac618bc368fed6b0d740738a271211c2c
ASKavyashree <askavyashree22@gmail.com>
2025-12-22 19:45:56 +0530

    Adding the Object file

---
+++ DogObject.java
@@ -0,0 +1,31 @@
+public class DogObject {
+    static String sound;
+    String name;
+    int age;
+    String color;
+    String breed;
+
+    public DogObject(int Dogage , String Color){
+        age=Dogage;
+        color=Color;
+    }
+    public static void main(String args[]){
+        DogObject dg =  new DogObject(45,"white");
+        System.out.println(dg.getName());
+        System.out.println(dg.getColor());
+        System.out.println(dg.getAge());
+        System.out.println(sound());
+    }
+    public static String sound(){
+        return sound;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getColor(){
+        return color;
+    }
+}
