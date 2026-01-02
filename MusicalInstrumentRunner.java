2aaa3ab0553e798e9b4995e291705ecd813d5d31
ASKavyashree <askavyashree22@gmail.com>
2026-01-02 22:51:07 +0530

    Adding a MusicalInstrument

---
+++ MusicalInstrumentRunner.java
@@ -0,0 +1,10 @@
+public class MusicalInstrumentRunner {
+    public static void main(String main[]){
+        MusicalInstrumentInheritance musicalinheritance = new MusicalInstrumentInheritance();
+        musicalinheritance.setDrum("drum");
+        musicalinheritance.setGuitarSound("solo");
+        System.out.println(musicalinheritance.getGuitarSound());
+        System.out.println(musicalinheritance.getPianoSound());
+
+    }
+}
