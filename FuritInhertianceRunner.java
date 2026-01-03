4aff7a18363e1805e7f3817c4044eb75ce70ad3a
ASKavyashree <askavyashree22@gmail.com>
2026-01-03 12:32:20 +0530

    Adding a new file

---
+++ FuritInhertianceRunner.java
@@ -0,0 +1,9 @@
+public class FuritInhertianceRunner {
+    public static void main(String args[]){
+        FruitInhertiance fruit=new FruitInhertiance();
+        fruit.setColor("Red");
+        System.out.println(fruit.getColor());
+        fruit.setName("Orange");
+        System.out.println(fruit.getName());
+    }
+}
