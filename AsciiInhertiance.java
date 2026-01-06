cf5b1a982c066322eb503b3f0e9b11e907aef0fd
ASKavyashree <askavyashree22@gmail.com>
2026-01-06 11:04:02 +0530

    Adding a new file

---
+++ AsciiInhertiance.java
@@ -0,0 +1,26 @@
+public class AsciiInhertiance extends  Ascii {
+    int a=12;
+    char b='A';
+
+    public AsciiInhertiance(){
+
+    }
+    public AsciiInhertiance(int a,char b){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+
+    public void setA(int a){
+        this.a=a;
+    } 
+    public int getA(){
+        return a;
+    }
+    public void setB(char b){
+        this.b=b;
+    }
+    public char getB(){
+        return b;
+    }
+}
