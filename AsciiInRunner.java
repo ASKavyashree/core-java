db80be3fd4158193eb7fef2476b1bfe1a69d18eb
ASKavyashree <askavyashree22@gmail.com>
2026-01-06 11:06:13 +0530

    Adding a new file

---
+++ AsciiInRunner.java
@@ -0,0 +1,10 @@
+public class AsciiInRunner {
+    public static void main(String args[]){
+        AsciiInhertiance ascii= new AsciiInhertiance();
+        ascii.setA(23);
+        System.out.println(ascii.getA());
+        ascii.setB('5');
+        System.out.println(ascii.getB());
+        System.out.println(ascii.getC());
+    }
+}
