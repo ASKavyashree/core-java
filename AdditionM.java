309a5174a4c4f0ba11c0bb7e37504d06c4affbf7
ASKavyashree <askavyashree22@gmail.com>
2026-01-08 19:40:05 +0530

    Adding a newfile

---
+++ AdditionM.java
@@ -0,0 +1,12 @@
+public class AdditionM extends Addition{
+    
+    public int getAdd(int b){
+        if(a==b){
+            System.out.println("equals");
+        }
+        else if(b>a){
+            System.out.println("B is greater than A");
+        }
+        return result;
+    }
+}
