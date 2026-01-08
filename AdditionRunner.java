3641d72ebce01ee644625e9a73755d2e5723bba2
ASKavyashree <askavyashree22@gmail.com>
2026-01-08 19:42:13 +0530

    Adding a new file

---
+++ AdditionRunner.java
@@ -0,0 +1,7 @@
+public class AdditionRunner {
+    public static void main(String args[]){
+        AdditionM addition = new AdditionM();
+        System.out.println(addition.getAdd());
+        System.out.println(addition.getAdd());
+    }
+}
