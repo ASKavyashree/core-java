d695b8b98b35808af6b425520e868e5182a1ce20
ASKavyashree <askavyashree22@gmail.com>
2026-01-08 22:14:54 +0530

    Adding a new file

---
+++ thingRunner.java
@@ -0,0 +1,7 @@
+public class thingRunner {
+    public static void main(String[] args){
+        thingM thing = new thingM();
+        System.out.println(thing.getThing("notebook"));
+        System.out.println(thing.getThing("textbook"));
+    }
+}
